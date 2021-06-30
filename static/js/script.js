15f4673c1edcd5ab6c1fa9892293d427ce9f3f6f
Sojasmine <sojasmine.gjerstad@gmail.com>
2021-06-30 19:19:39 +0000

    Add a container at jobs.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+  /* mobile sidenav bar*/
     $('.sidenav').sidenav({edge: "right"});
+    $('.collapsible').collapsible();
+  /* for the all jobs page*/
+ 
+    
   });
-     
