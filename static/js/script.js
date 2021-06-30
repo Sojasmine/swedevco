ea6700bf2692ce72a7267ea73c76464f84be0493
Sojasmine <sojasmine.gjerstad@gmail.com>
2021-06-30 21:46:53 +0000

    Add form on post_jobs.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
   /* mobile sidenav bar*/
     $('.sidenav').sidenav({edge: "right"});
+     /* for the all jobs page*/
     $('.collapsible').collapsible();
-  /* for the all jobs page*/
-  
+  /* for the post_jobs page*/
+  $('.datepicker').datepicker({
+    format: "dd mmm, yyyy",
+    yearRange: 3,
+    showClearBtn: true,
+    i18n: {
+      done: "Select"
+    }
+  });
+$('input#input_text, textarea#textarea2').characterCounter();
     
   });
